f14a856498db04b021bf9731d9f14628f6821c2c
Robert Metcalf <rmetcalf9@googlemail.com>
2021-08-18 18:57:34 +0100

    httpcall provider implemented

--- services/src/Providers/HttpCall.py
+++ services/src/Providers/HttpCall.py
@@ -1,11 +1,51 @@
 from ._baseProvider import ProviderBaseClass
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+import json
+
+supportedMethods = [requests.get, requests.post, requests.put, requests.options]
 
 class HttpCallProvider(ProviderBaseClass):
+  authCredentials = None
+
+  def _processInitData(self, config):
+    authCredentials = None
+    if "basicAuthCredentialFile" in config:
+      if config["basicAuthCredentialFile"] is not None:
+        if len(config["basicAuthCredentialFile"]) > 0:
+          if not os.path.isfile(config["basicAuthCredentialFile"]):
+            raise Exception("HTTPCall - Can not find basicAuthCredentialFile - " + config["basicAuthCredentialFile"])
+          with open(config["basicAuthCredentialFile"], 'r') as file:
+            val = file.read()
+          try:
+            valDict = json.loads(val)
+          except Exception:
+            raise Exception("HTTPCall - Not valid JSON - " + config["basicAuthCredentialFile"])
+          self.authCredentials = HTTPBasicAuth(valDict["username"], valDict["password"])
+          print("HTTPCall - Basic auth credentials setup")
+
+  def _getSupportedMethodAndUrl(self, value):
+    for x in supportedMethods:
+      if value.startswith(x.__name__ + ":"):
+        return (x, value[len(x.__name__)+1:])
+    return (None, None)
+
+  def _validateReciever(self, reciever, isFinal):
+    if reciever is None:
+      if isFinal:
+        return False
+      return True # we allow non override
+    (method, url) = self._getSupportedMethodAndUrl(reciever)
+    if method is None:
+      raise Exception("HTTPCall - reciever should start with a supported method - " + reciever)
+
+    return True
 
 
   def _processMessage(self, sender, receiver, subject, body, destination, bodyDict, tenantConfig, outputFn):
     if receiver is None:
       raise Exception("HTTPCall - message has no receiver")
-
-
-    raise Exception("HTTPCall TODO")
+    (method, url) = self._getSupportedMethodAndUrl(receiver)
+    result = requests.post(url, json=body, auth=self.authCredentials)
+    outputFn("HTTPCall message -> subject:" + str(subject) + " got response code " + str(result.status_code))
--- services/src/Providers/_baseProvider.py
+++ services/src/Providers/_baseProvider.py
@@ -17,50 +17,73 @@ class ProviderBaseClass:
     self.type = providerConfig["type"]
     self.recieverOverride = None
     if "receiverOverride" in providerConfig:
-      if not self._validateReciever(providerConfig["receiverOverride"]):
+      if not self._validateReciever(providerConfig["receiverOverride"],isFinal=False):
         raise Exception("Invalid receiverOverride")
       self.recieverOverride = providerConfig["receiverOverride"]
     self.senderOverride = None
     if "senderOverride" in providerConfig:
-      if not self._validateSender(providerConfig["senderOverride"]):
+      if not self._validateSender(providerConfig["senderOverride"],isFinal=False):
         raise Exception("Invalid senderOverride")
       self.senderOverride = providerConfig["senderOverride"]
+
+    self._processInitData(providerConfig["config"])
 
   def getId(self):
     return self.id
 
-  def _validateReciever(self, reciever):
-    return True
-  def _validateSender(self, sender):
-    return True
-
   def processMessage(self, destination, bodyDict, tenantConfig, outputFn):
+    #For overrides the override value has precedence over the message value
     receiver = None
     if self.recieverOverride is not None:
       receiver = self.recieverOverride
     elif "receiver" in bodyDict:
       receiver = bodyDict["receiver"]
     sender = None
-    if not self._validateReciever(receiver):
+    if not self._validateReciever(receiver,isFinal=True):
       raise Exception("Invalid Receiver")
 
     if self.senderOverride is not None:
       sender = self.senderOverride
     elif "receiver" in bodyDict:
       sender = bodyDict["sender"]
-    if not self._validateSender(sender):
+    if not self._validateSender(sender,isFinal=True):
       raise Exception("Invalid Sender")
+
+
+    def readString(bodyDict, key):
+      if key not in bodyDict:
+        return None
+      if not isInstance(bodyDict[key], str):
+        raise Exception(key + " of message should be string")
+      if bodyDict == "":
+        return None # empty strings changed to none
+      return bodyDict[key]
+
+    subject = readString(bodyDict, "subject")
+    body = readString(bodyDict, "body")
 
     self._processMessage(
       sender=sender,
       receiver=receiver,
-      subject="TODO",
-      body="TODO",
+      subject=subject,
+      body=body,
       destination=destination,
       bodyDict=bodyDict,
       tenantConfig=tenantConfig,
       outputFn=outputFn
     )
+
+  # Functions to override below
+
+  def _processInitData(self, config):
+    pass
+
+  #For the override, isFinal=False, for the final validation isFinal is true
+  def _validateReciever(self, reciever, isFinal):
+    return True
+  def _validateSender(self, sender, isFinal):
+    return True
+
 
   def _processMessage(self, sender, receiver, subject, body, destination, bodyDict, tenantConfig, outputFn):
     raise Exception("Provider should override process message function")
